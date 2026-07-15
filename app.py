2e0aadfcdf4e160d92375fc71801bc8a6f1871cd
Sayuri <i2413259@continental.edu.pe>
2026-07-15 00:28:26 -0500

    Descripción de los cambios 2

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask
 from database.db import get_connection
+from routes.producto_routes import producto_bp
 
 app = Flask(__name__)
+
+# Registrar Blueprints
+app.register_blueprint(producto_bp)
 
 
 @app.route("/")
 def inicio():
-
     try:
-
         conexion = get_connection()
 
         with conexion.cursor() as cursor:
-
             cursor.execute("SELECT VERSION()")
-
             version = cursor.fetchone()
 
         conexion.close()
@@ -22,11 +22,12 @@ def inicio():
         return f"""
         <h2>✅ Conexión exitosa</h2>
         <p>Versión MySQL: {version['VERSION()']}</p>
+        <hr>
+        <a href="/productos">Ir al módulo de productos</a>
         """
 
     except Exception as e:
-
-        return f"<h3>Error:</h3><p>{e}</p>"
+        return f"<h3>❌ Error de conexión</h3><p>{e}</p>"
 
 
 if __name__ == "__main__":
